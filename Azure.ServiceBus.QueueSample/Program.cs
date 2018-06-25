1d8ad0e3dc55770301aac90611c382394c24256e
antrad <antonio@nextgenerationteam.com>
2018-06-25 16:07:15 +0300

    Added more samples

---
+++ Azure.ServiceBus.QueueSample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.ServiceBus.Messaging;
+using QueueClient = Microsoft.Azure.ServiceBus.QueueClient;
+
+namespace Azure.ServiceBus.QueueSample
+{
+    class Program
+    {
+        //private const string QueueConnectionString = "Endpoint=sb://ngtservicebus1.servicebus.windows.net/;SharedAccessKeyName=policy1;SharedAccessKey=HrvXLi0wiHnVh9nYLVFilOeGf5TXQ341ZNMr0pvt7Xk=;EntityPath=ngtqueue1";
+        
+        static void Main(string[] args)
+        {
+            /*var client = new QueueClient(QueueConnectionString);
+            var message = new BrokeredMessage("Ciao");
+            message.Label = "Sample";
+            client.Send(message);*/
+        }
+    }
+}
---
+++ Azure.ServiceBus.TopicSample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Azure.ServiceBus.TopicSample
+{
+    class Program
+    {
+        private const string TopicConnectionString = "Endpoint=sb://ngtservicebus1.servicebus.windows.net/;SharedAccessKeyName=policy1;SharedAccessKey=HrvXLi0wiHnVh9nYLVFilOeGf5TXQ341ZNMr0pvt7Xk=;EntityPath=ngt";
+        
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
